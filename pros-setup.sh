cf1c7c7375f5012a76c0f647a012c5799be3716d
Lucas Borowiecki <baconomist@gmail.com>
2019-03-23 12:02:34 -0400

    Initial Project Setup. - Fixed CmakeLists.txt for cpp

---
+++ pros-setup.sh
@@ -0,0 +1,7 @@
+
+# Install c/c++ compilers
+sudo apt-get install build-essential
+sudo apt install gcc-arm-none-eabi
+
+sudo python3.6 -m pip install https://github.com/purduesigbots/pros-cli/releases/download/3.1.4/pros_cli_v5-3.1.4-py3-none-any.whl
+prosv5 --version # Test if installed
