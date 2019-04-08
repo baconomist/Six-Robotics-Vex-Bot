164b0ebff16a4fede06f5eab945fca0f16aaebd1
eekaba <6968281+eekaba@users.noreply.github.com>
2019-04-08 15:11:32 -0400

    Update opcontrol.cpp

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "okapi/api.hpp"
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -13,19 +14,30 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+ using namespace okapi;
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
-	}
-}
+ auto myChassis = ChassisControllerFactory::create(
+   {1, 9}, // Left motors
+   {-2, -10},   // Right motors
+   AbstractMotor::gearset::blue, // Torque gearset
+   {4_in, 12.5_in} // 4 inch wheels, 12.5 inch wheelbase width
+ );
+
+ auto profileController = AsyncControllerFactory::motionProfile(
+   0.25,  // Maximum linear velocity of the Chassis in m/s
+   2.0,  // Maximum linear acceleration of the Chassis in m/s/s
+   10.0, // Maximum linear jerk of the Chassis in m/s/s/s
+   myChassis // Chassis Controller
+ );
+
+ void opcontrol() {
+     okapi::Rate rate;
+     rate.delayUntil(1000);
+   profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{1_ft, 0_ft, 0_deg}}, "A");
+   profileController.generatePath({Point{1_ft, 0_ft, 0_deg}, Point{1_ft, 0_ft, 90_deg}}, "B");
+   profileController.setTarget("A");
+   profileController.waitUntilSettled();
+   profileController.setTarget("B");
+   profileController.waitUntilSettled();
+     pros::lcd::print(1, "GGGGGGGGGGGGGG");
+ }
