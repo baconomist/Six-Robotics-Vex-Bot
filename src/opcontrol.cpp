4c4b96e631e29dc1af4535d94b0609dbc0ef5a5b
Lucas Borowiecki <baconomist@gmail.com>
2019-09-16 15:24:23 -0400

    Crappy vision sensor range finding code

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,8 +1,15 @@
 
+#include <algorithm>
 #include "main.h"
 #include "headers/motors.h"
 #include "headers/controllers.h"
 #include "headers/motor_gearsets.h"
+
+#define VISION_PORT 1
+// Colors in order from top-bottom orange, purple, green
+#define ORANGE_SIG 1
+#define PURPLE_SIG 2
+#define GREEN_SIG 3
 
 using namespace std;
 void tank()
@@ -82,12 +89,47 @@ void transmission()
     }
 }
 
+/* At 1m:
+ * w 36px h 34px
+ * x 156px y 124px
+ * */
+
+vision_object_s_t object;
 void opcontrol()
 {
+    float distance_to_robot_edge = 0;
+    float y= 0;
+    float fov_in = 0; // To be calculated using cube_px_width_in_view / cube_px_width_1m_away
+    float d = (fov_in / 400)*y + distance_to_robot_edge;
+
+    float timer = millis();
+    float average_time = 1000; // 5 sec
+    float average_total;
+    int average_count = 0;
+
+    float values[999];
+
+    pros::Vision vision_sensor(VISION_PORT);
     while (true)
     {
-        arcade();
-        transmission();
+        float x = vision_sensor.get_by_sig(0, 3).width;
+        average_total +=  36 / x;
+        values[average_count] = 36 / x;
+        average_count ++;
 
+        if(millis() - timer >= average_time)
+        {
+            std::cout << "The average is: " << average_total / average_count << " " << *max_element(values, values + 999) << "\n";
+            timer = millis();
+            average_total = 0;
+            average_count = 0;
+            memset(values, 0, sizeof values);
+        }
+
+       // arcade();
+       // transmission();
+       object = vision_sensor.get_by_sig(0, 2);
+       //std::cout << object.width << "\n";
+       delay(50);
     }
 }
