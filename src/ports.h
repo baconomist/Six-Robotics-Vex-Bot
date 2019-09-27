37ff4fb187b07f94d943938f694037473c813fb6
Jack Walmsley <thefrenchjob@gmail.com>
2019-09-27 15:54:38 -0400

    Moved all port definitons to ports.h

---
+++ src/ports.h
@@ -0,0 +1,18 @@
+#define VISION_PORT 5
+
+// Drive
+#define LEFT_FRONT 20
+#define LEFT_BACK 10
+#define RIGHT_FRONT 11
+#define RIGHT_BACK 1
+
+#define TRANSMISSION_BOTTOM 9
+#define TRANSMISSION_TOP 2
+
+// Legacy Ports (A=1, B=2...)
+#define LEFT_X_ENCODER_TOP 1
+#define LEFT_X_ENCODER_BOTTOM 2
+#define RIGHT_X_ENCODER_TOP 3
+#define RIGHT_X_ENCODER_BOTTOM 4
+#define Y_ENCODER_TOP 5
+#define Y_ENCODER_BOTTOM 6
