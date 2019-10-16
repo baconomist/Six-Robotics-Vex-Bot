e7defa055748e4a33259815d2c082287f7f92d0d
AryanG123 <argajelli@gmail.com>
2019-10-16 16:58:07 -0400

    printing to controller

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -4,13 +4,16 @@
 #include "main.h"
 #include "robot/robot.h"
 #include <stdarg.h>
-
-
+#include "robot/controllers.h"
+#include "robot/motion_control/PID.h"
 void opcontrol()
 {
-  robot->robotMode = REMOTE_CONTROLLED;
- // robot->start_mainloop();
-  while(true){
-      robot->update();
-  }
+    robot->robotMode = REMOTE_CONTROLLED;
+    // robot->start_mainloop();
+    char buffer[20];
+    while(true){
+        robot->update();
+        sprintf(buffer,"%f",Kp);
+        master.set_text(1, 0, buffer);
+    }
 }
--- src/robot/motion_control/PID.h
+++ src/robot/motion_control/PID.h
@@ -8,6 +8,6 @@
 const int MIN_ERROR_FOR_INTEGRAL = 500;
 const int MIN_ERROR_RANGE = 10;
 // Constants used for scaling, range(0, 1)
-const float Kp = 0.5;
-const float Ki = 0.2;
-const float Kd = 0.1;
+float Kp = 0.5;
+float Ki = 0.2;
+float Kd = 0.1;
