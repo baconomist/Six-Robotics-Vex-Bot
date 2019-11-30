d256a918026df23cf86c03082374f2cd60ebe26c
Aryan Gajelli <argajelli@gmail.com>
2019-11-29 21:09:54 -0500

    This one allows to printf tray and lift pot pos to the lcd

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -10,29 +10,17 @@
 #include "robot/motion_control/PID.h"
 #include "robot/old/move_to_point.h"
 
-ADIPotentiometer tray(TRAY_POT);
-ADIPotentiometer lift(LIFT_POT);
+
 
 void opcontrol()
 {
     Robot::robotMode = ROBOT_MODE_RC;
     Drive::driveMode = DRIVE_MODE_ARCADE;
-    // robot->start_mainloop();
-    tray.calibrate();
-    lift.calibrate();
-    pros::delay(500);
-
-    //initialize_mtp(20, 20);
-
-    // Auton::goto_pos(0, 20);
-    // Auton::goto_pos(20, 20);
-    // Auton::goto_pos(20, 0);
-    // Auton::goto_pos(0, 0);
 
     while (true)
     {
         Robot::update();
-        printf("%d %d\n", tray.get_value_calibrated_HR(), lift.get_value_calibrated_HR());
+        //printf("%d %d\n", tray.get_value_calibrated_HR(), lift.get_value_calibrated_HR());
         //Auton::print_debug();
         //update_mtp();
         pros::delay(20);
