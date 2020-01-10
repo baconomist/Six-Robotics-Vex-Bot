509ebcfff12106ee96e5e0c20e3c654c8c07f67e
Jack <jackawalmsley@gmail.com>
2020-01-10 18:29:26 -0500

    Began cleaning mechanisms

---
+++ cmake-build-debug/CMakeFiles/VexRobot.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VexRobot.dir/src/auton_editor_lib/auton_path_parser.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/autonomous.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/initialize.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/opcontrol.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/components/drive.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/components/mechanisms.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/components/motor_gearsets.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/components/old_mechanisms.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/motion_control/PID.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/motion_control/auton.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/motion_control/kalman_filter.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/old/motion_tracker.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/old/move_to_point.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/old/move_to_point_simple.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/robot/robot.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/utils/action_queue.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/utils/math.cpp.obj"
+  "CMakeFiles/VexRobot.dir/src/utils/vector2.cpp.obj"
+  "VexRobot.exe"
+  "VexRobot.exe.manifest"
+  "VexRobot.pdb"
+  "libVexRobot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VexRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/VexRobot.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VexRobot.
+# This may be replaced when dependencies are built.
