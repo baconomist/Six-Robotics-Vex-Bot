c3ec93e3f4ea433be5aad64eafae55d982b8aae9
Unknown <baconomist@gmail.com>
2020-01-17 15:56:22 -0500

    More PROS project setup

---
+++ src/globals.h
@@ -0,0 +1,12 @@
+//
+// Created by Lucas on 1/17/2020.
+//
+
+#ifndef VEXROBOT_GLOBALS_H
+#define VEXROBOT_GLOBALS_H
+
+#include "main.h"
+
+#define master pros::Controller(pros::E_CONTROLLER_MASTER)
+
+#endif //VEXROBOT_GLOBALS_H
