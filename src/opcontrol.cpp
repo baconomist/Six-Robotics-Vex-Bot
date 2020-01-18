e1f56adc63a6fbc82ccfe198bbd788dda16a55de
Unknown <baconomist@gmail.com>
2020-01-17 19:41:58 -0500

    Dev Branch Created. Example commit to dev branch.
    If the code looks like this, it goes on the DEV BRANCH.

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 #include "okapi/api.hpp"
-#include "globals.h"
-#include "ports.h"
+#include "../src/globals.h"
+#include "../src/ports.h"
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -17,8 +17,54 @@
  * task, not resume it from where it left off.
  */
 
+using namespace okapi::literals;
+using namespace okapi;
+
 void opcontrol()
 {
+    okapi::ADIEncoder leftEncoder(LEFT_Y_ENCODER_TOP, LEFT_Y_ENCODER_BOTTOM);
+    okapi::ADIEncoder rightEncoder(RIGHT_Y_ENCODER_BOTTOM, RIGHT_Y_ENCODER_TOP, false);
+    okapi::ADIEncoder centerEncoder(X_ENCODER_BOTTOM, X_ENCODER_TOP, false);
 
+    IterativePosPIDController::Gains distanceGains;
+    distanceGains.kP = 0.0005;
+    distanceGains.kI = 0;
+    distanceGains.kD = 0.00000;
+
+    IterativePosPIDController::Gains turnGains;
+    turnGains.kP = 0.0005;
+    turnGains.kI = 0;
+    turnGains.kD = 0.00000;
+
+    IterativePosPIDController::Gains angleGains;
+    angleGains.kP = 0.0005;
+    angleGains.kI = 0;
+    angleGains.kD = 0.00000;
+
+
+    auto chassisController = ChassisControllerBuilder()
+            .withMotors(LEFT_FRONT, -RIGHT_FRONT, -RIGHT_BACK, LEFT_BACK)
+            .withSensors(
+                    leftEncoder,
+                    rightEncoder,
+                    centerEncoder
+            )
+            .withGains(distanceGains, turnGains, angleGains)
+            .withDimensions(okapi::AbstractMotor::gearset::green, {{3.25_in, 16_in}, okapi::imev5GreenTPR})
+            .withOdometry({{3.25_in, 16_in}, okapi::quadEncoderTPR}, StateMode::CARTESIAN)
+            .buildOdometry();
+
+    Point point;
+    point.x = 0_ft;
+    point.y = 1_ft;
+    printf("aaaa");
+    //chassisController->moveDistance(12_in);
+    //chassisController->waitUntilSettled();
+    //chassisController->moveDistance(-12_in);
+    chassisController->driveToPoint(point);
+    chassisController->waitUntilSettled();
+    printf("Settled");
+    //chassisController->turnAngle(okapi::degree * 90);
+    //chassisController->moveDistance(-okapi::inch * 12);
 }
 
